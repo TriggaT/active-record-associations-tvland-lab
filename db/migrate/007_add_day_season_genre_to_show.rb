3942d119122df40c569c8777843f893aff92ac9f
Trayshawn <traytwebb14@yahoo.com>
2020-12-16 18:24:18 -0600

    commit tree 31500de62dcb168e2cfdbb15b93558f641daaa9c

---
+++ db/migrate/007_add_day_season_genre_to_show.rb
@@ -0,0 +1,13 @@
+class AddDaySeasonGenreToShow < ActiveRecord::Migration[6.0]
+  
+  
+    def change
+  
+      add_column :shows, :day, :string
+      add_column :shows, :season, :string
+      add_column :shows, :genre, :string
+      
+  
+    end
+  end
+  
